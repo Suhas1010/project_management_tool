297aabf542b388dca3862ad253c296d52e158ffb
Suhas Hatti <hattisuhas@gmail.com>
2026-08-16 17:31:40 +0530

    add project controllers and models

---
+++ src/models/project.models.js
@@ -0,0 +1,22 @@
+import mongoose,{Schema} from "mongoose";
+import { ref } from "process";
+
+const projectSchema = ({
+    name :{
+        type : String,
+        required : true,
+        unique : true,
+        trim : true
+    },
+    description:{
+        type : String,
+    },
+    createdBy :{
+        type : Schema.Types.ObjectId,
+        ref : "User",
+        required : true
+    }
+},
+{ timestamps: true,}
+)
+export const Project = mongoose.model("Project",projectSchema)
